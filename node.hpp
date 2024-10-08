4724b56df0ee076fb6d437125d252b12781b65df
redmonsteroid <danilzagibalov@yandex.ru>
2024-10-09 00:07:46 +0700

    start

---
+++ node.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+struct Node {
+    std::string data;
+    Node* next;
+    Node* prev;
+
+    Node(const std::string& value, Node* nextNode = nullptr, Node* prevNode = nullptr)
+        : data(value), next(nextNode), prev(prevNode) {
+    }
+};
